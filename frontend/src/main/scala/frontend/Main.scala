4a3006cc1526dfad416381c878173a3c0b972e51
ex0ns <g.tournigand@gmail.com>
2023-12-08 08:18:57 +0100

    Laminar signal's delay

--- frontend/src/main/scala/frontend/Main.scala
+++ frontend/src/main/scala/frontend/Main.scala
@@ -2,6 +2,7 @@ package frontend
 
 import org.scalajs.dom
 import com.raquo.laminar.api.L.*
+import com.raquo.laminar.codecs.AsIsCodec
 import com.raquo.laminar.nodes.*
 import com.raquo.laminar.keys.*
 import com.raquo.laminar.tags.*
@@ -23,11 +24,17 @@ object Container:
 
 @main
 def main =
-  val app = Container(
-    cls := "custom-class scala",
-    child <-- Val(div(cls:= "test scala", slot := "header", "Fill the header")),
-    child <-- Val(div(cls:= "test2 scala", slot := "header", "Fill the header (also)")),
-    child <-- Val(Embed(slot := "default")),
-    child <-- Val(div(cls:= "scala", slot := "footer", "Fill the footer"))
+
+  val app = div(
+    Container(
+      cls := "custom-class scala",
+      // htmlProp("name", AsIsCodec()) := "name",
+      htmlProp("name", AsIsCodec()) <-- Signal.fromValue("name from signal"),
+      child <-- EventStream.periodic(1000).map(n => div(cls:= "test scala", slot := "header", f"Counter $n")),
+      child <-- Val(div(cls:= "test2 scala", slot := "header", "Fill the header (also)")),
+      child <-- Val(Embed()),
+      // child <-- Val(div("try me")),
+      child <-- Val(div(cls:= "scala", slot := "footer", "Fill the footer"))
+    )
   )
   renderOnDomContentLoaded(dom.document.querySelector("#app"), app)
--- src/components/Container.vue
+++ src/components/Container.vue
@@ -3,12 +3,26 @@
     <div class="somethingelse">
       <slot name="header">Missing header</slot>
       <span>Right before the slot</span>
-      <slot></slot>
+      <slot name="default">default slot default content</slot>
       <span>Right after the slot</span>
       <slot name="footer">Missing footer</slot>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+    }
+  },
+  created() {
+    console.log(this.name);
+    console.log("created");
+  }
+}
+</script>
 
 <style scoped>
 .container {
